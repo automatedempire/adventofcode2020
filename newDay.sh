a068ba5674b6ecba594c961334a7beb41a3a37e4
Adam Vincent <automatedempire@users.noreply.github.com>
2020-12-06 13:25:09 -0500

    Adding more standard boilerplate to the new day script, and making it executable

--- newDay.sh
+++ newDay.sh
@@ -8,4 +8,5 @@ py_file=${fname}.py
 
 wget --load-cookies cookies.txt https://adventofcode.com/2020/day/$day/input -O ${puzzle_input}
 
-echo -e "#!/usr/bin/env python3\n\npuzzle_input = '${puzzle_input}'" > ${py_file}
+echo -e "#!/usr/bin/env python3\n\npuzzle_input = '${puzzle_input}'\n\nwith open(puzzle_input, 'r') as puzz_in:\n" > ${py_file}
+chmod 755 ${py_file}
