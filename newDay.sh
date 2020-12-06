8182b0ae2132c31f41479e01a6141b4a4a26f927
Adam Vincent <automatedempire@users.noreply.github.com>
2020-12-06 10:18:21 -0500

    Adding helper script for starting the new day

---
+++ newDay.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+day=$1
+day_format=$(printf "%02d" $day)
+fname=day${day_format}
+puzzle_input="input/$fname"
+py_file=${fname}.py
+
+wget --load-cookies cookies.txt https://adventofcode.com/2020/day/$day/input -O ${puzzle_input}
+
+echo -e "#!/usr/bin/env python3\n\npuzzle_input = '${puzzle_input}'" > ${py_file}
